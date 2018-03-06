6ba178f0e75167b381bef40f088620f6a488852b
Christian Miller <christian.ax.miller@gmail.com>
2018-03-05 18:59:59 -0800

    ran npm i, wrote in server file

--- lab-christian/server.js
+++ lab-christian/server.js
@@ -1,2 +1,30 @@
 'use strict';
 
+'use strict';
+
+const express = require('express');
+const debug = require('debug')('cfgram:server');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const authRouter = require('./route/auth-router');
+const errors = require('./lib/error-middleware');
+
+dotenv.load();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(authRouter);
+app.use(errors);
+
+const server = module.exports = app.listen(PORT, () => {
+  debug(`Server up on PORT ${PORT}`);
+});
+
+server.isRunning = true;
